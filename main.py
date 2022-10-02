ede5c8a2c31143cdf0d548d6bf7be77797a0abc3
Ayush Agarwal <95439195+ayush-agarwal-190@users.noreply.github.com>
2022-10-02 20:26:18 +0530

    Function Weather API, Time and place
    Added function weather , time , place

--- main.py
+++ main.py
@@ -113,5 +113,44 @@ if __name__ == "__main__":
         elif "log off" in statement or "sign out" in statement:
             speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
             subprocess.call(["shutdown", "/l"])
+	def digital_assistant(data):
+#     global listening
+   elif "how are you" in data:
+        listening = True
+        respond("I am well")
+
+    elif "what time is it" in data:
+        listening = True
+        respond(ctime())
+
+    elif "where is" in data:
+        listening = True
+        data = data.split(" ")
+        location_url = "https://www.google.com/maps/place/" + str(data[2])
+        respond("Hold on Dante, I will show you where " + data[2] + " is.")
+        maps_arg = '/usr/bin/open -a "/Applications/Google Chrome.app" ' + location_url
+        os.system(maps_arg)
+        
+    elif "what is the weather in" in data:
+        listening = True
+        api_key = "Your_API_key"
+        weather_url = "http://api.openweathermap.org/data/2.5/weather?"
+        data = data.split(" ")
+        location = str(data[5])
+        url = weather_url + "appid=" + api_key + "&q=" + location 
+        js = requests.get(url).json() 
+        elif js["cod"] != "404": 
+            weather = js["main"] 
+            temp = weather["temp"] 
+            hum = weather["humidity"] 
+            desc = js["weather"][0]["description"]
+            resp_string = " The temperature in Kelvin is " + str(temp) + " The humidity is " + str(hum) + " and The weather description is "+ str(desc)
+            respond(resp_string)
+        else: 
+            respond("City Not Found") 
+    elif "stop listening" in data:
+        listening = False
+        print('Listening stopped')
+    return listening
 			
 time.sleep(3)
